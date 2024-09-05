ab399535128f3be90fcbf5d83b978dfdc6442c61
Adebayo Philip <114924407+coderrrrr2@users.noreply.github.com>
2024-09-05 11:14:42 +0100

    added body parser

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
+
+
 const app = express();
+
+app.use(bodyParser.urlencoded());
 
 
 
 app.use('/add-product',(req, res, next) => {
-    console.log('In the another middleware');
-    res.send('<h1>Add Product Page</h1>');
 
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+
+});
+
+app.post('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 
 app.use('/',(req, res, next) => {
-    console.log('In the another middleware');
+
     res.sendFile(__dirname + '/index.html');
 
 });
