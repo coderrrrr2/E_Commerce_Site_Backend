4d34e006bd74ec5283bb143cf831b8fcc2b54d39
Adebayo Philip <114924407+coderrrrr2@users.noreply.github.com>
2024-09-11 22:55:45 +0100

    made changes

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
 const express = require('express');
 
-const bodyParser = require('body-parser');
-
-
-
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded());
 
+app.use('/admin',adminRoutes);
 
+app.use(shopRoutes);
 
-app.use('/add-product',(req, res, next) => {
-
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-
-});
-
-app.post('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/',(req, res, next) => {
-
-    res.sendFile(__dirname + '/index.html');
-
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
