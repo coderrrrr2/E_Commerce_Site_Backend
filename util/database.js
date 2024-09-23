d763eb3ca19fa5cb4587f44dd693292c1c078237
Adebayo Philip <114924407+coderrrrr2@users.noreply.github.com>
2024-09-23 12:59:01 +0100

    updates

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 
 
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 
 const sequelize = new Sequelize({
-  dialect: PostgresDialect,
+    dialect: 'postgres',
   database: 'adebayophilip',
   user: 'adebayophilip',
   password: 'password',
