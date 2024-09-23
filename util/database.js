940456184351d652b5720507a6070fe01e9cc0c7
Adebayo Philip <114924407+coderrrrr2@users.noreply.github.com>
2024-09-23 12:42:23 +0100

    fixed sequelize installation error

--- util/database.js
+++ util/database.js
@@ -1,28 +1,16 @@
-const { Pool } = require('pg');
 
-// Create a pool instance
-const pool = new Pool({
-  user: 'adebayophilip',
-  host: 'localhost',
+
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize({
+  dialect: PostgresDialect,
   database: 'adebayophilip',
+  user: 'adebayophilip',
   password: 'password',
+  host: 'localhost',
   port: 5432,
-});
+  clientMinMessages: 'notice',
+})
 
-
-
-
-// const connectToDatabase = () => {
-//   return new Promise((resolve, reject) => {
-//     pool.connect((err, client, release) => {
-//       if (err) {
-//         reject('Error acquiring client', err.stack);
-//       } else {
-//         console.log("Connected to PostgreSQL database");
-//         resolve({ client, release }); // Return both client and release function
-//       }
-//     });
-//   });
-// };
-
-module.exports = pool;
+module.exports = sequelize;
